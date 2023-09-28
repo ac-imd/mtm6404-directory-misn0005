8a690d330e5ed18210c2a86314b5a32a4bb6f493
Dante Misner <misn0005@algonqinlive.com>
2023-09-28 09:19:05 -0400

    adds content to functions.js

---
+++ functions.js
@@ -0,0 +1,60 @@
+//list function
+function list(clients) {
+    const listItems = clients.map((client) => `
+      <div class="list-group-item" data-index="${client.index}">
+        ${client.name}
+       ${client.balance}
+      </div>
+    `);
+
+    return listItems.join('');
+}
+
+
+
+//order function
+function order(clients, property) {
+    clients.sort((a, b) => {
+        if (a[property] < b[property]) {
+            return -1;
+        } else if (a[property] > b[property]) {
+            return 1;
+        } else {
+            return 0;
+        }
+    });
+
+    return clients;
+}
+
+
+
+//total function
+function total(clients) {
+    const totalBalance = clients.reduce((accumulator, client) => {
+        return accumulator + client.balance;
+    }, 0);
+
+    return totalBalance;
+}
+
+
+
+
+//info function
+function info(index) {
+    const client = clients.find((client) => {
+        return client.index === index;
+    });
+}
+
+
+
+//search function
+function search(query) {
+    const results = clients.filter((client) => {
+        return client.name.toLowerCase().includes(query.toLowerCase());
+    });
+
+    return results;
+}
